89f2b3e5399ec1b2e7859020dee049c0738edcae
aruga <ryoh.aruga@nts.ricoh.co.jp>
2017-12-04 18:22:47 +0900

    [update]チェックボックスによる判断文を追加

--- js/control.js
+++ js/control.js
@@ -2,6 +2,47 @@ var self = this;
 
 function create(){
     var input_text = $("#input_textbox").val();
-    $("#output_textbox").val(input_text);
     console.log(input_text);
+    console.log("checkbox_name:" + $("#checkbox_name").is(':checked'));
+    console.log("checkbox_date:" + $("#checkbox_date").is(':checked'));
+    console.log("checkbox_edited:" + $("#checkbox_edited").is(':checked'));
+    console.log("checkbox_br:" + $("#checkbox_br").is(':checked'));
+
+    if($("#checkbox_name").is(':checked')){
+        console.log("delete name");
+        self.deleteNames(input_text);
+    }
+
+    if($("#checkbox_date").is(':checked')){
+        console.log("delete dates");
+        self.deleteDateWords(input_text);
+    }
+
+    if($("#checkbox_edited").is(':checked')){
+        console.log("delete edited word");
+        self.deleteEditedWords(input_text);
+    }
+
+    if($("#checkbox_br").is(':checked')){
+        console.log("delete br");
+        self.deleteBRs(input_text);
+    }
+
+    $("#output_textbox").val(input_text);
+}
+
+function deleteNames(val){
+    console.log("[deleteNames]"+val);
+}
+
+function deleteDateWords(val){
+
+}
+
+function deleteEditedWords(val){
+
+}
+
+function deleteBRs(val){
+    
 }
