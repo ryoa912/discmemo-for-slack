ab93a26aadca9807457ea2949020c08e6e246d07
aruga <ryoh.aruga@nts.ricoh.co.jp>
2017-12-08 14:19:50 +0900

    [update]JSの文字列変換処理を追加

--- js/control.js
+++ js/control.js
@@ -1,7 +1,44 @@
 var self = this;
 
 function create(){
-    var input_text = $("#input_textbox").val();
+    var input_text = "";
+    input_text = $("#input_textbox").val();
+
+    if($("#checkbox_name").is(':checked')){
+        input_text = self.deleteNames(input_text);
+    }
+
+    if($("#checkbox_date").is(':checked')){
+        input_text = self.deleteDateWords(input_text);
+    }
+
+    if($("#checkbox_edited").is(':checked')){
+        input_text = self.deleteEditedWords(input_text);
+    }
+
+    if($("#checkbox_br").is(':checked')){
+        input_text = self.deleteBRs(input_text);
+    }
+
     $("#output_textbox").val(input_text);
-    console.log(input_text);
+}
+
+function deleteNames(val){    
+    var text = val;
+    return text.replace( /\n(.+)\[/g , "[" );
+}
+
+function deleteDateWords(val){
+    var text = val;
+    return text.replace( /\[(.+)\]/g , "" );
+}
+
+function deleteEditedWords(val){
+    var text = val;
+    return text.replace( /\(edited\)/g , "" );
+}
+
+function deleteBRs(val){
+    var text = val;
+    return text.replace( /(\n\s*)+/g , "\n" );
 }
